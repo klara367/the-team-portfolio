483d78b8ef01ac52280fd3fb9c82073e9798cc75
Klara <k>
2023-06-02 19:21:53 +0200

    Filled details page with data.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom"
 import Layout from "./components/Layout"
 import Portfolio from "./pages/Portfolio"
 import Error from "./pages/Error"
+import ProjectDetails from "./pages/ProjectDetails"
 
 const App = () => {
   return(
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Portfolio />} />
+        <Route path="/details/:name" element={<ProjectDetails />}/>
         <Route path="*" element={<Error />}/>
       </Route>
     </Routes>
--- src/pages/ProjectDetails.js
+++ src/pages/ProjectDetails.js
@@ -1,6 +1,35 @@
+import { useParams } from "react-router-dom"
+import { useState } from "react"
+import data from "../data.json"
+
 const ProjectDetails = () => {
+    let { name } = useParams()
+
+    let projectDetails = data.find(p => p.name === name)
+
+    const { anotation, demo, details, documentation, images, marketing } = projectDetails
+
     return (
         <main>
+            <section>
+                <h2>{name}</h2>
+                <p>{anotation}</p>
+                <p>{details}</p>
+                <div>
+                    <a href={demo}>Demo</a>
+                    <a href={documentation}>Dokumentace</a>
+                    <a href={marketing}>Marketing</a>
+                </div>
+            </section>
+            <section>
+                <div>
+                    {images && images.map((img, index) => {
+                        return <div key={index}>
+                            <img src={img.image} alt={img.title} />
+                        </div>
+                    })}
+                </div>
+            </section>
 
         </main>
     )
