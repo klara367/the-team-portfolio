e6f3937eef31c21eee9ed07b1e696408cce622ee
Klara <k>
2023-06-03 09:56:58 +0200

    Header Styling.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import MainLogo from "../assets/MainLogo.png"
+import "./Header.css"
 
 const Header = () => {
     return (
-        <>
+        <header>
             <h1>Porfolio Produktů</h1>
             <img src={MainLogo} alt="Logo" />
-        </>
+        </header>
     )
 }
 
