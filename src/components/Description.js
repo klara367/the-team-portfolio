63f42745dfa79a72a2a7f830fc13c1f7062492e3
Klara <k>
2023-06-04 09:55:16 +0200

    Corrected responsiveness.

--- src/components/Description.js
+++ src/components/Description.js
@@ -2,14 +2,16 @@ import "./Description.css"
 
 const Description = ({ demo, details, documentation, images, marketing, name }) => {
     return (
-        <section className="desription-wrapper">
-            <img src={images} alt="project preview" />
-            <p className="fw-600 name">{name}</p>
-            <p className="details">{details}</p>
-            <div className="btn-wrapper">
-                <a href={demo} className="description-btn">Demo</a>
-                <a href={documentation} className="description-btn">Dokumentace</a>
-                <a href={marketing} className="description-btn">Marketing</a>
+        <section>
+            <div className="desription-wrapper">
+                <img src={images} alt="project preview" />
+                <p className="fw-600 name">{name}</p>
+                <p className="details">{details}</p>
+                <div className="btn-wrapper">
+                    <a href={demo} className="description-btn">Demo</a>
+                    <a href={documentation} className="description-btn">Dokumentace</a>
+                    <a href={marketing} className="description-btn">Marketing</a>
+                </div>
             </div>
         </section>
     )
