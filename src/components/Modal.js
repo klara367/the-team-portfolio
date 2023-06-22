e485ffa1e2b5f5af323e637209cc2b824e8884d3
Klara <k>
2023-06-22 19:06:01 +0200

    Download button

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,7 @@
 import { IoIosArrowBack, IoIosArrowForward, IoMdClose } from "react-icons/io"
 import "./Modal.css"
+import { saveAs } from "file-saver"
+import { BsDownload } from "react-icons/bs"
 
 const Modal = ({ imgNumber, setImgNumber, image, title, images, setOpenModal }) => {
 
@@ -15,6 +17,10 @@ const Modal = ({ imgNumber, setImgNumber, image, title, images, setOpenModal }) 
         imgNumber + 1 === images.length ? setImgNumber(0) : setImgNumber(imgNumber + 1)
     }
 
+    const downloadImg = () => {
+        saveAs(image, `${title}.png`)
+    }
+
     return (
         <div className="modal flex-center">
             <IoMdClose className='close-btn' onClick={handleCloseModal}  />
@@ -22,7 +28,7 @@ const Modal = ({ imgNumber, setImgNumber, image, title, images, setOpenModal }) 
             <IoIosArrowForward  className='next-btn' onClick={nextImg} />
             <div className="fullscreen-img-wrapper flex-center">
                 <img src={image} alt={title} className="fullscreen-img-wrapper__img"/>
-                <p className="fullscreen-img-wrapper__p">{title}</p>
+                <p className="fullscreen-img-wrapper__p"> <span>{title} </span><BsDownload  className="download-icon" onClick={downloadImg} /></p>
             </div>
         </div>
     )
